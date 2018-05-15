6b0d1e7db0dfcb4698d96476d2e92447999b1c81
animah88 <animahwolf@gmail.com>
2018-05-15 13:47:12 +0300

    add Money.cs

---
+++ Lab2/Money.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Money
+    {
+        public Money()
+        {
+            Console.WriteLine("Purse constructor");
+            Console.ReadKey();
+        }
+    }
+}
